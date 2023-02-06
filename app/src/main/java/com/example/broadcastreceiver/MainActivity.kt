3465c6db950e2a8e587df2cd29c870d3bb8584c9
Alyona <borisovsk_a@mail.ru>
2023-02-06 13:25:35 +0300

    feat: Add broadcast receiver programmatically

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.broadcastreceiver
 
+import android.Manifest.permission.RECEIVE_SMS
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 1
+    }
+
+    // Создали экземпляр receiver
+    private val smsReceiver = MySmsReceiver()
+
+    // Создали intent filter
+    private val filter = IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(RECEIVE_SMS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(RECEIVE_SMS),
+                MY_PERMISSIONS_REQUEST_RECEIVE_SMS)
+        } else {
+            // Если разрешения есть, регистрируем receiver
+            this.registerReceiver(smsReceiver, filter)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Разрегистрируем
+        this.unregisterReceiver(smsReceiver)
     }
 }
---
+++ app/src/main/java/com/example/broadcastreceiver/MySmsReceiver.kt
@@ -0,0 +1,23 @@
+package com.example.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.widget.Toast
+
+class MySmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val intentAction = intent.action
+
+        if (intentAction != null) {
+            when (intentAction) {
+                Telephony.Sms.Intents.SMS_RECEIVED_ACTION ->
+                    Toast.makeText(context, "Sms received", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
